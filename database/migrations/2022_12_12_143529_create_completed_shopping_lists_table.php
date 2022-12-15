1ea74faf81eba03369cd493a726bfcd059b8811f
tamura masumi <tamuramasumi@example.com>
2022-12-15 12:01:53 +0000

    エラー

--- database/migrations/2022_12_12_143529_create_completed_shopping_lists_table.php
+++ database/migrations/2022_12_12_143529_create_completed_shopping_lists_table.php
@@ -14,7 +14,7 @@ class CreateCompletedShoppingListsTable extends Migration
     public function up()
     {
         Schema::create('completed_shopping_lists', function (Blueprint $table) {
-            $table->unsignedInteger('id');
+            $table->unsignedInteger>id();
             $table->string('name', 128)->comment('「買うもの」名');
             $table->unsignedBigInteger('user_id')->comment('この「買うもの」の所有者');
             $table->foreign('user_id')->references('id')->on('users'); // 外部キー制約
@@ -22,7 +22,7 @@ class CreateCompletedShoppingListsTable extends Migration
             $table->dateTime('created_at')->useCurrent()->comment('購入日時');
             $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
             //
-            $table->primary('id');
+            /*$table->primary('id');*/
         });
     }
 
